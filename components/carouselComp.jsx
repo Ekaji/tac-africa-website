679f9f3d18c4c4e302222c0075f240e2fa82c127
Ekaji <ekajionah@gmail.com>
2022-03-15 13:25:21 +0100

    feature: created carucel component

--- components/carouselComp.jsx
+++ components/carouselComp.jsx
@@ -5,58 +5,155 @@ import Carousel from 'react-bootstrap/Carousel'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
 import styles from '../styles/component.styles/getInvolved.module.scss'
+import Link from 'next/link'
 
 
 export default function CarouselComp() {
     return (
-      <Carousel variant="dark">
-        <Carousel.Item interval={1000}>
-          <div className='row'>
-            <div className='col-md-6' >
-              <div className={ styles.labs_card } >
-                <div>
-                  <img className="img-fluid" src="./drone-lab-icon.png" alt="" />
+      <div className={styles.carousel_container} >
+        <div>
+            <h2 className={ styles.homepage_title } >How you can get 
+              <button className={styles.homepage_button}>
+                <em>
+                  <Link href='/contact'  >
+                    <a>
+                    Involved
+                    </a>
+                  </Link>
+                </em>
+              </button>
+            </h2>
+        </div>
+        <Carousel variant="dark">
+          <Carousel.Item className={styles.carousel_items_padding} interval={1000}>
+            <div className='row'>
+              <div className='col-md-6' >
+                <div className={ styles.labs_card } >
+                  <div>
+                    <img className="img-fluid" src="./drone-lab-icon.png" alt="" />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='col-md-6' >
-            <div className={ styles.labs_card } >
-                <div>
-                  <h3>Drone and Counter-Drone Lab</h3>
-                  <p>A Virtual knowledge platform that will raise critical issues to promote the institutionalization
-                      of drone and counter drone best practices within the African Sub-region. </p>
+              <div className='col-md-6' >
+              <div className={ styles.labs_card } >
+                  <div>
+                    <h3>Drone and Counter-Drone Lab</h3>
+                    <p>A Virtual knowledge platform that will raise critical issues to promote the institutionalization
+                        of drone and counter drone best practices within the African Sub-region. </p>
 
-                  <Nav.Item className={ styles.button_comp } >
+                    <Nav.Item className={ styles.button_comp } >
+                        <Button variant="outline-primary" >Learn More</Button>
+                    </Nav.Item>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item className={styles.carousel_items_padding} >
+            <div className='row'>
+              <div className='col-md-6' >
+                <div className={ styles.labs_card } >
+                  <div>
+                    <img className="img-fluid" src="/syber-lab-icon.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-6' >
+                <div className={ styles.labs_card } >
+                  <div>
+                    <h3>Cyber Security Lab</h3>
+                    <p>Lorem ipsum dorlo Lorem ipsum dorlo Lorem ipsum
+                      dorlo Lorem i psum dorlo Lorem ipsum dorlo Lorem
+                      ipsum dorlo Lorem ipsum dorlo Lorem ipsum
+                      dorlo Lorem i psum dorlo Lorem ipsum dorlo 
+                    </p>
+                    <Nav.Item className={ styles.button_comp } >
                       <Button variant="outline-primary" >Learn More</Button>
-                  </Nav.Item>
+                    </Nav.Item>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className='row'>
-            <div className='col-md-6' >
-              <div className={ styles.labs_card } >
-                <div>
-                  <img className="img-fluid" src="./drone-lab-icon.png" alt="" />
+          </Carousel.Item>
+          <Carousel.Item className={styles.carousel_items_padding} >
+            <div className='row'>
+              <div className='col-md-6' >
+                <div className={ styles.labs_card } >
+                  <div>
+                    <img className="img-fluid" src="/syber-lab-icon.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-6' >
+                <div className={ styles.labs_card } >
+                  <div>
+                    <h3>AI and Robotics Lab</h3>
+                    <p>Lorem ipsum dorlo Lorem ipsum dorlo Lorem ipsum
+                      dorlo Lorem i psum dorlo Lorem ipsum dorlo Lorem
+                      ipsum dorlo Lorem ipsum dorlo Lorem ipsum
+                      dorlo Lorem i psum dorlo Lorem ipsum dorlo 
+                    </p>
+                    <Nav.Item className={ styles.button_comp } >
+                      <Button variant="outline-primary" >Learn More</Button>
+                    </Nav.Item>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className='col-md-6' >
-              <div className={ styles.labs_card } >
-                <div>
-                  <h3>Drone and Counter-Drone Lab</h3>
-                  <p>A Virtual knowledge platform that will raise critical issues to promote the institutionalization
-                  of drone and counter drone best practices within the African Sub-region. </p>
-                  <Nav.Item className={ styles.button_comp } >
-                    <Button variant="outline-primary" >Learn More</Button>
-                  </Nav.Item>
+          </Carousel.Item>
+          <Carousel.Item className={styles.carousel_items_padding} >
+            <div className='row'>
+              <div className='col-md-6' >
+                <div className={ styles.labs_card } >
+                  <div>
+                    <img className="img-fluid" src="/syber-lab-icon.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-6' >
+                <div className={ styles.labs_card } >
+                  <div>
+                    <h3>Blockchain and Crypto Lab</h3>
+                    <p>Lorem ipsum dorlo Lorem ipsum dorlo Lorem ipsum
+                      dorlo Lorem i psum dorlo Lorem ipsum dorlo Lorem
+                      ipsum dorlo Lorem ipsum dorlo Lorem ipsum
+                      dorlo Lorem i psum dorlo Lorem ipsum dorlo 
+                    </p>
+                    <Nav.Item className={ styles.button_comp } >
+                      <Button variant="outline-primary" >Learn More</Button>
+                    </Nav.Item>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Carousel.Item>
-      </Carousel>
+          </Carousel.Item>
+          <Carousel.Item className={styles.carousel_items_padding} >
+            <div className='row'>
+              <div className='col-md-6' >
+                <div className={ styles.labs_card } >
+                  <div>
+                    <img className="img-fluid" src="/syber-lab-icon.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-6' >
+                <div className={ styles.labs_card } >
+                  <div>
+                    <h3>Satellite And Remote Sensing Lab</h3>
+                    <p>Lorem ipsum dorlo Lorem ipsum dorlo Lorem ipsum
+                      dorlo Lorem i psum dorlo Lorem ipsum dorlo Lorem
+                      ipsum dorlo Lorem ipsum dorlo Lorem ipsum
+                      dorlo Lorem i psum dorlo Lorem ipsum dorlo 
+                    </p>
+                    <Nav.Item className={ styles.button_comp } >
+                      <Button variant="outline-primary" >Learn More</Button>
+                    </Nav.Item>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     );
 }
