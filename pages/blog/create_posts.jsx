9e020fbd9ff223be1397a868a48d56b0c028e75c
Ekaji <ekajionah@gmail.com>
2022-05-20 15:57:30 +0100

    update to blog posts routes

--- pages/blog/create_posts.jsx
+++ pages/blog/create_posts.jsx
@@ -28,7 +28,7 @@ function CreatePost() {
           { title, content, user_id: user.id, user_email: user.email }
       ])
       .single()
-    router.push(`/posts/${data.id}`)
+    router.push(`/blog/${data.id}`)
   }
 
   const user = supabase?.auth?.currentUser?.email;
